f78b0d8937c3ae1f569b017ae309c7a1f915f2f2
VashchenkoA <61081442+VashchenkoA@users.noreply.github.com>
2020-02-16 18:53:08 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,44 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const WCHAR *fileName = L"\\\\.\\H:";
+
+    HANDLE fileHandle = CreateFileW(
+        fileName,
+        GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (fileHandle == INVALID_HANDLE_VALUE)
+    {
+        cout << "Error" << endl;
+    }
+
+    LARGE_INTEGER sectorOffset;
+    sectorOffset.QuadPart = 0;
+
+    unsigned long currentPosition = SetFilePointer(
+        fileHandle,
+        sectorOffset.LowPart,
+        &sectorOffset.HighPart,
+        FILE_BEGIN
+    );
+
+    if (currentPosition != sectorOffset.LowPart)
+    {
+        cout << "Error" << endl;
+    }
+
+    CloseHandle(fileHandle);
+
+    return 0;
+
+};
